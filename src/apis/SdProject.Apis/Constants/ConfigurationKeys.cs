a999131d317fdfa3348e5ec705c0ef9f30d3961a
Linh Nguyen <linh.nguyen94@outlook.com.vn>
2022-09-05 21:59:55 +0700

    - Removed redundant files.

--- src/apis/SdProject.Apis/Constants/ConfigurationKeys.cs
+++ src/apis/SdProject.Apis/Constants/ConfigurationKeys.cs
@@ -4,17 +4,7 @@
     {
         #region Properties
 
-        public const string IdentityServer = "IdentityServer";
-
-        public const string Cors = "Cors";
-
         public const string Swagger = "swagger";
-
-        public const string Redis = "Redis";
-
-        public const string MassTransit = "MassTransit";
-
-        public const string Auth0Config = "AuthZero";
 
         #endregion
     }
--- src/apis/SdProject.Businesses/Cqrs/Commands/Users/AddUserCommand.cs
+++ src/apis/SdProject.Businesses/Cqrs/Commands/Users/AddUserCommand.cs
@@ -6,7 +6,6 @@ namespace SdProject.Businesses.Cqrs.Commands.Users
 {
     public class AddUserCommand : IRequest<User>
     {
-        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime Birthdate { get; set; }
